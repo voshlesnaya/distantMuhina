4e3fb6afb804519160af7b8b818d46bf50151d28
voshlesnaya <petrovsmysenkodenis@gmail.com>
2026-02-27 09:21:26 +0500

    dddd

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    @vite['resources/css/app.css', 'resources/js/app.js']
+
     <header>
         шапка 
     </header>
